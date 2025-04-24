50d0298d06972687acac0294e47ec4143fcce551
Sergio <sergiomurcia01@gmail.com>
2025-04-24 13:14:43 -0600

    actualizacion config

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import sqlite3
 
+'''
 def conexion_BD():
     conexion = sqlite3.connect('Data/biblioteca.db')
     conexion.row_factory = sqlite3.Row
 
     return conexion
-
 '''
+
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "Data", "biblioteca.db")
@@ -14,7 +15,7 @@ DB_PATH = os.path.join(BASE_DIR, "Data", "biblioteca.db")
 def conexion_BD():
     return sqlite3.connect(DB_PATH)
 
-'''
+
 
 
 
