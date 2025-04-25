3b7d654dda38a9df012d5d57fdad376e8d6d10ed
Sergio <sergiomurcia01@gmail.com>
2025-04-25 10:12:08 -0600

    Actualización de config.py y requirements.txt

--- config.py
+++ config.py
@@ -13,11 +13,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "Data", "Biblioteca.db")
 
 def conexion_BD():
+    print("Ruta BD:", DB_PATH)  # O loguearlo con logging
     return sqlite3.connect(DB_PATH)
 
 
 
 
-
-
-#
