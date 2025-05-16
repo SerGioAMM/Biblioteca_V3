e9faaf0d95b1b3bb462e92a53611e97e7fb2e1eb
SerGioAMM <sergiomurcia01@gmail.com>
2025-05-16 12:33:31 -0600

    Actualizacion de base de datos

--- static/js/obtener_fecha.js
+++ static/js/obtener_fecha.js
@@ -5,7 +5,7 @@ const mm = String(hoy.getMonth() + 1).padStart(2, '0'); // los meses van de 0 a 
 const dd = String(hoy.getDate()).padStart(2, '0');
 const fechaHoy = `${yyyy}-${mm}-${dd}`;
 
-// Obtener la fecha dentro de 7 días
+// Obtener la fecha dentro de 7 dias
 const sieteDias = new Date(hoy);
 sieteDias.setDate(hoy.getDate() + 7);
 const yyyy7 = sieteDias.getFullYear();
@@ -13,6 +13,7 @@ const mm7 = String(sieteDias.getMonth() + 1).padStart(2, '0');
 const dd7 = String(sieteDias.getDate()).padStart(2, '0');
 const fechaSiete = `${yyyy7}-${mm7}-${dd7}`;
 
-// Asignar valores
+
+
 document.getElementById('fecha_prestamo').value = fechaHoy;
 document.getElementById('fecha_entrega_estimada').value = fechaSiete;
