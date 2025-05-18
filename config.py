20f71e27de319ae2d71f4d5af95c6e5de527d065
SerGioAMM <sergiomurcia01@gmail.com>
2025-05-18 15:01:26 -0600

    Correccion de algunas cosas responsive

--- config.py
+++ config.py
@@ -1,25 +1,8 @@
 import sqlite3
 
-'''
-
-def conexion_BD():
-    conexion = sqlite3.connect('Data/Biblioteca_GM.db')
-    return conexion
-
-
-
-
-'''
-
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "Data", "Biblioteca_GM.db")
 
 def conexion_BD():
     return sqlite3.connect(DB_PATH)
-
-
-
-
-
-#    conexion.row_factory = sqlite3.Row
